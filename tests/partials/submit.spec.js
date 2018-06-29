297900234139ff6b777f06072e2f9dbd55cb4293
Liam Keaton <liamkeaton@gmail.com>
2018-06-29 16:10:01 +0100

    Adding b2b options and partials  🐿 v2.9.0

--- tests/partials/submit.spec.js
+++ tests/partials/submit.spec.js
@@ -22,4 +22,33 @@ describe('submit template', () => {
 
 		expect($('button').text()).to.equal(label);
 	});
+
+	it('should not display description by default', () => {
+		const $ = context.template({});
+
+		expect($('p').length).to.equal(0);
+	});
+
+	it('should display the given description', () => {
+		const description = 'This is a testing description';
+		const $ = context.template({
+			description
+		});
+
+		expect($('p').text()).to.equal(description);
+	});
+
+	it('should not be centered by default', () => {
+		const $ = context.template({});
+
+		expect($('.ncf__field--center').length).to.equal(0);
+	});
+
+	it('should be centered if set', () => {
+		const $ = context.template({
+			isCentered: true
+		});
+
+		expect($('.ncf__field--center').length).to.equal(1);
+	});
 });
