a11bedb637bc5c7092b78aab2d8e2f3a8a6547ed
Liam Keaton <liamkeaton@gmail.com>
2018-09-17 09:27:27 +0100

    Tracking utility  🐿 v2.9.0

---
+++ utils/tracking.js
@@ -0,0 +1,97 @@
+const imageUrl = 'https://spoor-api.ft.com/px.gif';
+const eventName = 'oTracking.event';
+
+class Tracking {
+	/**
+	 *
+	 * @param {Window} window Window object to access Image and CustomEvent on
+	 * @param {Element} element HTML element to dispatch event on, normally document.body
+	 */
+	constructor (window, element) {
+		if (!window || !element) {
+			throw new Error('Please supply a window and element');
+		}
+
+		this.window = window;
+		this.element = element;
+		this.initDebugData();
+	}
+
+	/**
+	 * Construct and dispatch a standard tracking event, falls back to dispacting tracking pixel
+	 * @param {string} category
+	 * @param {string} action
+	 * @param {object} data
+	 * @returns {number} Amount of events dispatched
+	 */
+	dispatch (category, action, data = {}) {
+		if (!category || !action) {
+			throw new Error('Please supply a category and action');
+		}
+
+		const eventData = Object.assign({}, data, { action, category });
+
+		try {
+			return this.dispatchEventAsCustomEvent(eventData);
+		} catch (e) {
+			return this.dispatchEventAsImage(eventData);
+		}
+	}
+
+	/**
+	 * Fire a CustomEvent on the given element with tracking data
+	 * @param {object} data
+	 * @returns {number} Amount of events dispatched
+	 */
+	dispatchEventAsCustomEvent (data) {
+		const event = new this.window.CustomEvent(eventName, {
+			bubbles: true,
+			cancelable: true,
+			data
+		});
+
+		this.element.dispatchEvent(event);
+		return this.addDebugData(data);
+	}
+
+	/**
+	 * Load a tracking pixel with encoded tracking data
+	 * @param {object} data
+	 * @returns {number} Amount of events dispatched
+	 */
+	dispatchEventAsImage (data) {
+		const image = new this.window.Image();
+		const encodedData = encodeURIComponent(JSON.stringify(data));
+
+		image.src = `${imageUrl}?data=${encodedData}`;
+		return this.addDebugData(data);
+	}
+
+	/**
+	 * Initalise debug data array
+	 * @returns {array}
+	 */
+	initDebugData () {
+		return this.window.debugTracking = this.window.debugTracking || [];
+	}
+
+	/**
+	 * Add a new debug message
+	 * @param {object} data
+	 * @returns {number} Number of debug messages
+	 */
+	addDebugData (data) {
+		this.window.debugTracking.push({ time: new Date(), data });
+		return this.window.debugTracking.length;
+	}
+
+	/**
+	 * Return all the debug data
+	 * @returns {array}
+	 */
+	getDebugData () {
+		return this.window.debugTracking || [];
+	}
+};
+
+module.exports = Tracking;
