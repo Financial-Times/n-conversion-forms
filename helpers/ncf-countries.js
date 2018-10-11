40fea7efa8e1c1d21626f2b152a6c544888084ff
Alex Naish <alex@naish.io>
2018-10-11 13:59:18 +0100

    Upgrade countries  🐿 v2.8.0

--- helpers/ncf-countries.js
+++ helpers/ncf-countries.js
@@ -1,9 +1,9 @@
-const countries = require('n-common-static-data').billingCountries;
+const { countries } = require('n-common-static-data').billingCountries;
 
 module.exports = function ({ hash = {}, fn }) {
-	this.commonData = this.commonData || {};
-	this.commonData.countries = Array.isArray(hash.filterList) ? countries.filter(countryInFilterList(hash.filterList)) : countries;
-	return fn(this);
+	const data = Array.isArray(hash.filterList) ? countries.filter(countryInFilterList(hash.filterList)) : countries;
+	const context = Object.assign({ countries: data }, this);
+	return fn(context);
 };
 
 function countryInFilterList (filterList) {
