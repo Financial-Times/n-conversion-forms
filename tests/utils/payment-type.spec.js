e3f2ed2dae918611f2856db69a621f828597548c
Liam Keaton <liamkeaton@gmail.com>
2019-02-05 17:07:32 +0000

    Adding payment type utility  🐿 v2.10.3

---
+++ tests/utils/payment-type.spec.js
@@ -0,0 +1,105 @@
+const PaymentType = require('../../utils/payment-type');
+const expect = require('chai').expect;
+const sandbox = require('sinon').createSandbox();
+
+describe('PaymentType', () => {
+	let element;
+	let paymentTypeField;
+	let paymentType;
+	let document;
+
+	beforeEach(() => {
+		paymentTypeField = {
+			querySelector: () => element,
+			append: () => {}
+		};
+		element = {
+			remove: () => {},
+			cloneNode: () => element,
+			setAttribute: () => {},
+			parentElement: paymentTypeField
+		};
+		document = {
+			querySelector: () => {
+				return paymentTypeField;
+			}
+		};
+		paymentType = new PaymentType(document);
+		sandbox.spy(paymentType, 'show');
+		sandbox.spy(paymentType, 'hide');
+		sandbox.spy(paymentTypeField, 'append');
+		sandbox.spy(paymentTypeField, 'querySelector');
+		sandbox.spy(element, 'remove');
+		sandbox.spy(element, 'setAttribute');
+	});
+
+	afterEach(() => {
+		sandbox.restore();
+	});
+
+	describe('constructor', () => {
+		it('should throw an error if window not passed', () => {
+			expect(() => {
+				new PaymentType();
+			}).to.throw();
+		});
+
+		it('should throw an error if element not passed', () => {
+			expect(() => {
+				document.querySelector = () => false;
+				new PaymentType(document);
+			}).to.throw();
+		});
+	});
+
+	describe('toggle', () => {
+		it('should call show if true passed', () => {
+			paymentType.toggle(PaymentType.CREDITCARD, true);
+			expect(paymentType.show.calledOnce).to.be.true;
+		});
+
+		it('should call hide if false passed', () => {
+			paymentType.toggle(PaymentType.DIRECTDEBIT, false);
+			expect(paymentType.hide.calledOnce).to.be.true;
+		});
+	});
+
+	describe('show', () => {
+		it('should append two new elements', () => {
+			paymentType.show(PaymentType.CREDITCARD);
+			expect(paymentTypeField.append.callCount).to.equal(2);
+		});
+
+		it('should set the correct id', () => {
+			paymentType.show(PaymentType.CREDITCARD);
+			expect(element.setAttribute.calledWith('id', PaymentType.CREDITCARD)).to.be.true;
+		});
+
+		it('should set the correct value', () => {
+			paymentType.show(PaymentType.CREDITCARD);
+			expect(element.setAttribute.calledWith('value', PaymentType.CREDITCARD)).to.be.true;
+		});
+
+		it('should set the correct for', () => {
+			paymentType.show(PaymentType.CREDITCARD);
+			expect(element.setAttribute.calledWith('for', PaymentType.CREDITCARD)).to.be.true;
+		});
+
+		it('should set the correct label', () => {
+			paymentType.show(PaymentType.CREDITCARD);
+			expect(element.innerText).to.equal(PaymentType.LABELS[PaymentType.CREDITCARD]);
+		});
+	});
+
+	describe('hide', () => {
+		it('should remove two elements', () => {
+			paymentType.hide(PaymentType.PAYPAL);
+			expect(element.remove.callCount).to.equal(2);
+		});
+
+		it('should use the correct selector', () => {
+			paymentType.hide(PaymentType.PAYPAL);
+			expect(paymentTypeField.querySelector.calledWithMatch(PaymentType.PAYPAL)).to.be.true;
+		});
+	});
+});
