bf1a8d0bd3b17cb0045fdd48da4de9e7e6d13e36
Remy Bach <remy@bach.me.uk>
2020-01-21 09:28:34 +0000

    Remove Handlebars 🐿 v2.12.5

--- components/billing-city.spec.js
+++ components/billing-city.spec.js
@@ -1,10 +1,10 @@
 import { BillingCity } from './index';
-import { expectToRenderAs } from '../test-jest/helpers/expect-to-render-as';
+import { expectToRenderCorrectly } from '../test-jest/helpers/expect-to-render-correctly';
 import { fetchPartialAsString } from '../test-jest/helpers/fetch-hbs-as-string';
 
 const context = {};
 
-expect.extend(expectToRenderAs);
+expect.extend(expectToRenderCorrectly);
 
 describe('BillingCity', () => {
 	beforeAll(async () => {
@@ -14,24 +14,24 @@ describe('BillingCity', () => {
 	it('renders with default props', () => {
 		const props = {};
 
-		expect(BillingCity).toRenderAs(context, props);
+		expect(BillingCity).toRenderCorrectly(context, props);
 	});
 
 	it('renders with an error', () => {
 		const props = { hasError: true };
 
-		expect(BillingCity).toRenderAs(context, props);
+		expect(BillingCity).toRenderCorrectly(context, props);
 	});
 
 	it('renders with a custom value', () => {
 		const props = { value: 'foobar' };
 
-		expect(BillingCity).toRenderAs(context, props);
+		expect(BillingCity).toRenderCorrectly(context, props);
 	});
 
 	it('renders with a disabled input element', () => {
 		const props = { isDisabled: true };
 
-		expect(BillingCity).toRenderAs(context, props);
+		expect(BillingCity).toRenderCorrectly(context, props);
 	});
 });
--- components/package-change.spec.js
+++ components/package-change.spec.js
@@ -1,22 +1,15 @@
 import { PackageChange } from './index';
-import { expectToRenderAs } from '../test-jest/helpers/expect-to-render-as';
-import { fetchPartialAsString } from '../test-jest/helpers/fetch-hbs-as-string';
+import { expectToRenderCorrectly } from '../test-jest/helpers/expect-to-render-correctly';
 
-const context = {};
-
-expect.extend(expectToRenderAs);
+expect.extend(expectToRenderCorrectly);
 
 describe('PackageChange', () => {
-	beforeAll(async () => {
-		context.template = await fetchPartialAsString('package-change.html');
-	});
-
 	it('render with defaults', () => {
 		const props = {
 			changePackageUrl: 'https://www.ft.com',
 			currentPackage: 'Trial'
 		};
 
-		expect(PackageChange).toRenderAs(context, props);
+		expect(PackageChange).toRenderCorrectly(props);
 	});
 });
