29ca5d52d1b5f4bf2976801a16f3f48046f00d37
Stanimir Dimov <stanimir.dimov@ft.com>
2020-09-04 14:52:25 +0300

    Changed message in licence title when there is no company name presented

--- components/licence-title.jsx
+++ components/licence-title.jsx
@@ -27,14 +27,20 @@ export function LicenceTitle ({
 }
 
 function renderB2BTitle (displayName) {
-	const companyName = displayName ? displayName : 'Your company';
-	const boldTextClassName = (displayName !== '') ? 'ncf__bold-licence-text' : 'ncf__light-licence-text';
-	const template =
-		<h1 className="ncf__header ncf__center">
-			<span className="ncf__light-licence-text">Great news! </span>
-			<span className={boldTextClassName}>{companyName}</span>
-			<span className="ncf__light-licence-text"> has an FT Subscription you can join</span>
-		</h1>;
+	let template;
+	if(displayName) {
+		template =
+				<h1 className="ncf__header ncf__center">
+					<span className="ncf__light-licence-text">Great news! </span>
+					<span className="ncf__bold-licence-text">{displayName}</span>
+					<span className="ncf__light-licence-text"> has an </span>
+					<span className="ncf__bold-licence-text">FT subscription </span>
+					<span className="ncf__light-licence-text">you can join</span>
+            	</h1>;
+	} else {
+		template =
+				<h1 className="ncf__header ncf__center">Join your FT.com subscription</h1>;
+	}
 
 	return template;
 }
