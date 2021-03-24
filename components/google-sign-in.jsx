104b158f5723a8d768a49e496c8094bbe056f020
Matias Aguero <mjaguero.work@gmail.com>
2021-03-24 18:01:49 +0100

    ACQ-866: Adding a GoogleSignIn button component with styles.

---
+++ components/google-sign-in.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export function GoogleSignIn({ signInRedirectUrl }) {
+
+	return (
+		<>
+			<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet"></link>
+			<a className="google_button" href={signInRedirectUrl}>Sign in with Google</a>
+		</>
+	);
+}
+
+GoogleSignIn.propTypes = {
+	signInRedirectUrl: PropTypes.string.isRequired,
+};
