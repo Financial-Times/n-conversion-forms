2d1b2dd31149c1764fa5724de634c73da87b90df
Plamen Hristov <phristov@blocksoft.ltd>
2021-08-02 12:28:41 +0300

    Updated fieldId

--- components/personal-title.jsx
+++ components/personal-title.jsx
@@ -13,7 +13,7 @@ export function PersonalTitle ({
 	value,
 	isDisabled = false,
 	hasError = false,
-	fieldId = 'title',
+	fieldId = 'titleField',
 	selectId = 'title',
 	selectName = 'title',
 	options = defaultPersonalTitles,
