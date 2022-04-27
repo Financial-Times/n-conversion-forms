96c1beddcb22101e01ec81abbe5b38a861952b6a
Jordi Bueno <jordi.bueno.parser@ft.com>
2022-04-27 13:24:30 +0200

    ACQ-1505: Adds social media links to the confirmation page

--- components/registration-confirmation.jsx
+++ components/registration-confirmation.jsx
@@ -3,6 +3,25 @@ import PropTypes from 'prop-types';
 
 const EMAIL_DEFAULT_TEXT = 'your email';
 
+const SOCIALS = [
+	{
+		name: 'instagram',
+		link: 'https://www.instagram.com/financialtimes',
+	},
+	{
+		name: 'linkedin',
+		link: 'https://www.linkedin.com/company/financial-times',
+	},
+	{
+		name: 'twitter',
+		link: 'https://twitter.com/FT',
+	},
+	{
+		name: 'facebook',
+		link: 'https://www.facebook.com/financialtimes',
+	},
+];
+
 export function RegistrationConfirmation ({ email = EMAIL_DEFAULT_TEXT }) {
 	return (
 		<div className="ncf">
@@ -20,12 +39,32 @@ export function RegistrationConfirmation ({ email = EMAIL_DEFAULT_TEXT }) {
 				<div className="ncf__confirmation--socials">
 					<b>Breaking news alerts, direct to your lock screen</b>
 					<p className="ncf__confirmation--mobile">
-						Download our apps or follow us on our socials
+						{/* TODO: Once the store links are done, switch this sentence */}
+						{/* Download our apps or follow us on our socials */}
+						Find us in the app and google play stores or follow us on our
+						socials
 					</p>
 					<p className="ncf__confirmation--desktop">
 						Find us in the app and google play stores or follow us on our
 						socials
 					</p>
+					<section
+						className="o-social-follow"
+						aria-label="Follow on social media"
+					>
+						{SOCIALS.map(({ name, link }) => (
+							<a
+								key={name}
+								href={link}
+								className={`o-social-follow-icon o-social-follow-icon--${name}`}
+								target="_blank"
+								rel="noopener noreferrer"
+								data-trackable={`register-social-${name}`}
+							>
+								<span className="o-social-follow-icon__label">on {name}</span>
+							</a>
+						))}
+					</section>
 				</div>
 				<div className="ncf__confirmation--alerts">
 					<b>
@@ -40,7 +79,7 @@ export function RegistrationConfirmation ({ email = EMAIL_DEFAULT_TEXT }) {
 					<a
 						href={'/myft'}
 						className="margin-top-x4 ncf__button ncf__button--secondary"
-						data-trackable="personalise-alerts"
+						data-trackable="register-personalise-my-alerts"
 					>
 						Personalise my alerts
 					</a>
@@ -51,7 +90,7 @@ export function RegistrationConfirmation ({ email = EMAIL_DEFAULT_TEXT }) {
 				href={'/'}
 				target="_parent"
 				className="ncf__confirmation--finish ncf__button ncf__button--submit"
-				data-trackable="confirmation-finish"
+				data-trackable="register-finish"
 			>
 				Finish
 			</a>
