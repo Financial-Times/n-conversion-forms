d14be9c28e5d52a67c4d90094ed096935ab0cf39
Matias Aguero <mjaguero.work@gmail.com>
2022-05-25 11:40:14 +0200

    ACQ-1666: small change on accept terms

--- components/accept-terms.stories.js
+++ components/accept-terms.stories.js
@@ -11,8 +11,35 @@ export default {
 				options: ['immediate', 'endOfTerm'],
 			},
 		},
+		isAuthFirstAccount: { control: 'boolean' },
+		isAuthFirstPayment: { control: 'boolean' },
+		hasError: { control: 'boolean' },
+		isSignup: { control: 'boolean' },
+		isRegister: { control: 'boolean' },
+		isChecked: { control: 'boolean' },
+		isB2b: { control: 'boolean' },
+		isB2cPartnership: { control: 'boolean' },
+		isEmbedded: { control: 'boolean' },
+		isCorpSignup: { control: 'boolean' },
+		isTrial: { control: 'boolean' },
+		isTransition: { control: 'boolean' },
+		isPrintProduct: { control: 'boolean' },
+		isSingleTerm: { control: 'boolean' },
+		isNewDigitalBuyFlowConsent: { control: 'boolean' },
+		hideConfirmTermsAndConditions: { control: 'boolean' },
 	},
 };
 
 export const Basic = (args) => <AcceptTerms {...args} />;
-Basic.parameters = {};
+Basic.args = {};
+
+export const NewBuyFlow = (args) => <AcceptTerms {...args} />;
+NewBuyFlow.args = {
+	isAuthFirstAccount: true,
+};
+
+export const NewBuyFlowEmailVerification = (args) => <AcceptTerms {...args} />;
+NewBuyFlowEmailVerification.args = {
+	isAuthFirstAccount: true,
+	hideConfirmTermsAndConditions: true,
+};
