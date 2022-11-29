9156c927af1cab432c96be890f19b716b05c71c5
ludrob <robin.ludovic@gmail.com>
2022-11-29 11:22:24 +0000

    fix prettier/lint conflict rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,9 @@
+// we run prettier on commit and push, disabling some rules here that will conflict with prettier
 module.exports = {
 	extends: ['@financial-times/eslint-config-next'],
+	rules: {
+		'space-before-function-paren': 'off',
+		'quotes': 0,
+		'indent': 0,
+	},
 };
