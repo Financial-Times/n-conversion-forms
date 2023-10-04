2b306d87858be850784fa1d5a3a067a780d64a6e
ludrob <robin.ludovic@gmail.com>
2023-10-04 17:53:10 +0100

    remove mention of covid19 on paper voucher delivery option

--- secret-squirrel.js
+++ secret-squirrel.js
@@ -1,15 +1,16 @@
 module.exports = {
 	files: {
 		allow: [],
-		allowOverrides: [],
+		allowOverrides: []
 	},
 	strings: {
 		deny: [],
 		denyOverrides: [
-			'createB2cDiscountCopy',
-			'test@example\\.com', // components/__snapshots__/confirmation.spec.js.snap:544|606, components/__snapshots__/email.spec.js.snap:154|186, components/__snapshots__/registration-confirmation.spec.js.snap:15|62, components/confirmation.spec.js:29, components/email.spec.js:30, components/registration-confirmation.spec.js:21, docs/PARTIALS.md:106|357, test/partials/confirmation.spec.js:12, test/partials/registration-confirmation.spec.js:12, test/utils/email.spec.js:148|159|168|178|188
-			'd19dc7a6-c33b-4931-9a7e-4a74674da29a', // components/__snapshots__/licence-confirmation.spec.js.snap:28|62, components/licence-confirmation.spec.js:44
-			'addressLine3MaxLength', // components/delivery-address-map.jsx:63|138|198|132
-		],
-	},
+			'foo@bar\\.com', // components/__snapshots__/confirmation.spec.js.snap:184, components/confirmation.spec.js:10, components/confirmation.stories.js:44
+			'test@example\\.com', // components/__snapshots__/confirmation.spec.js.snap:529, components/__snapshots__/email.spec.js.snap:90, components/__snapshots__/registration-confirmation.spec.js.snap:12, components/confirmation.spec.js:33, components/email.spec.js:23, components/registration-confirmation.spec.js:14, utils/email.spec.js:153|165|176|190|204
+			'd19dc7a6\\x2dc33b\\x2d4931\\x2d9a7e\\x2d4a74674da29a', // components/__snapshots__/licence-confirmation.spec.js.snap:28, components/licence-confirmation.spec.js:37
+			'addressLine3MaxLength', // components/delivery-address-map.jsx:63|138|198, components/delivery-address.jsx:132
+			'createB2cDiscountCopy' // components/payment-term.jsx:196|308
+		]
+	}
 };
